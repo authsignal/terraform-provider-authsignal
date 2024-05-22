306f8041d18d21793a6df12a0cac587c29392c61
Jono Templeton <littlejono@icloud.com>
2024-05-22 16:40:07 +1200

    AUT-2035: updating tests

--- internal/provider/action_configuration_resource_test.go
+++ internal/provider/action_configuration_resource_test.go
@@ -43,6 +43,24 @@ func TestAccActionConfigurationResource(t *testing.T) {
 					resource.TestCheckResourceAttr("authsignal_action_configuration.test-templates", "messaging_templates", "{\"en\":{\"defaultTemplate\":\"hello world\"}}"),
 				),
 			},
+			{
+				Config: `
+					resource "authsignal_action_configuration" "test-verification-methods" {
+						action_code = "terraform-acceptance-test-verification-methods"
+						default_user_action_result = "ALLOW"
+						verification_methods = ["EMAIL_OTP", "EMAIL_MAGIC_LINK"]
+						default_verification_method = "EMAIL_MAGIC_LINK"
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "action_code", "terraform-acceptance-test-verification-methods"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "default_user_action_result", "ALLOW"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "tenant_id", "ec3910e7-ab32-479e-b58b-36a122631d58"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "verification_methods.0", "EMAIL_OTP"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "verification_methods.1", "EMAIL_MAGIC_LINK"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "default_verification_method", "EMAIL_MAGIC_LINK"),
+				),
+			},
 			// Update and Read testing
 			{
 				Config: `
@@ -74,6 +92,24 @@ func TestAccActionConfigurationResource(t *testing.T) {
 					resource.TestCheckResourceAttr("authsignal_action_configuration.test-templates", "default_user_action_result", "ALLOW"),
 					resource.TestCheckResourceAttr("authsignal_action_configuration.test-templates", "tenant_id", "ec3910e7-ab32-479e-b58b-36a122631d58"),
 					resource.TestCheckResourceAttr("authsignal_action_configuration.test-templates", "messaging_templates", "{\"fr\":{\"defaultTemplate\":\"bonjour\"}}"),
+				),
+			},
+			{
+				Config: `
+					resource "authsignal_action_configuration" "test-verification-methods" {
+						action_code = "terraform-acceptance-test-verification-methods"
+						default_user_action_result = "ALLOW"
+						verification_methods = ["AUTHENTICATOR_APP", "EMAIL_MAGIC_LINK"]
+						default_verification_method = "AUTHENTICATOR_APP"
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "action_code", "terraform-acceptance-test-verification-methods"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "default_user_action_result", "ALLOW"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "tenant_id", "ec3910e7-ab32-479e-b58b-36a122631d58"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "verification_methods.0", "AUTHENTICATOR_APP"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "verification_methods.1", "EMAIL_MAGIC_LINK"),
+					resource.TestCheckResourceAttr("authsignal_action_configuration.test-verification-methods", "default_verification_method", "AUTHENTICATOR_APP"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
